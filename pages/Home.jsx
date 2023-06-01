7fb259ee45a79f8de14d4c211373f309d95885c9
gitfname <newamailnam@gmail.com>
2023-06-01 23:20:19 +0430

    testing change

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,7 @@
 export default function HomePage() {
     return (
         <div className="w-screen h-screen bg-slate-800 grid place-items-center">
-            <p className="text-6xl text-white font-bold italic">Hello World</p>
+            <p className="text-6xl text-white font-bold italic">Hello World.</p>
         </div>
     )
 }
